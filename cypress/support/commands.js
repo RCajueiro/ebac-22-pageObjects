d23367149367a483754f9b8f339e2306220b202c
Ramon <ramoncajueiro@me.com>
2024-08-14 21:06:32 -0300

    Atualização commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -16,7 +16,7 @@
 Cypress.Commands.add('checkout', (nome, telefone, endereço, cidade, estado, cep) => { 
     cy.get('[data-testid="search-icon"]').click()
     cy.get('[data-testid="searchInput"]').click().type('Ingrid Running Jacket')
-    cy.get('#root').find('.css-146c3p1.r-cqee49').contains('Ingrid').click({force: true})
+    cy.get('[data-testid="productDetails"]').contains('Ingrid').click({force: true})
     cy.get('[data-testid="addToCart"]').click()
     cy.get('[data-testid="addItem"] > .css-146c3p1').click()
     cy.get('[data-testid="addNewAddress"] > .r-lrvibr').click()
