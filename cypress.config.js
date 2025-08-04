171775efc436e09bef4414807a6698e9ac63457e
Ramon <ramoncajueiro@me.com>
2025-08-04 02:25:44 -0300

    cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
+const { tr } = require("@faker-js/faker");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  projectId: "wjqs92",
+
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,11 +15,11 @@
 // -- This is a child command --
 Cypress.Commands.add('checkout', (nome, telefone, endereço, cidade, estado, cep) => { 
     cy.get('[data-testid="search-icon"]').click()
-    cy.get('[data-testid="searchInput"]').click().type('Ingrid Running Jacket')
-    cy.get('[data-testid="productDetails"]').contains('Ingrid').click({force: true})
+    cy.get('[data-testid="searchInput"]').click().type('Tênis Esportivo')
+    cy.get('[data-testid="productDetails"]').contains('Tênis').click({force: true})
     cy.get('[data-testid="addToCart"]').click()
     cy.get('[data-testid="addItem"] > .css-146c3p1').click()
-    cy.get('[data-testid="productName"]').first().should('have.text', 'Ingrid Running Jacket')
+    cy.get('[data-testid="productName"]').first().should('have.text', 'Tênis Esportivo')
     cy.get('[data-testid="addNewAddress"] > .r-lrvibr').click()
     cy.get('.r-1d7mnkm > :nth-child(1) > .css-175oi2r > .css-11aywtz').type(nome)
     cy.get(':nth-child(2) > .css-175oi2r > .css-11aywtz').type(telefone)
